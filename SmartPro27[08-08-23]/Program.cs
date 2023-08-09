f133010a0dfac6367740402fabb71bcc3399fedd
lesponns <adamadakadam1903@gmail.com>
2023-08-09 14:18:16 +0300

    Add project files.

---
+++ SmartPro27[08-08-23]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmartPro27_08_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input_fact, last = 1;
+
+            Console.Write("Faktoriyel alınacak sayı: ");
+            input_fact = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input_fact; i++)
+            {
+                last *= i;
+            }
+            Console.Write("\n{0}! = {1}",input_fact, last);
+            Console.ReadKey();
+        }
+    }
+}
